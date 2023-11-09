290aa03b6b7120b5809189a9a0678a2bbbbc90c7
tom-walker <walkertom1996@gmail.com>
2023-11-09 12:16:15 +0000

    navbar and footer files added to layout

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,6 @@
 import Hero from './Hero/Hero';
 import CustomButton from './CustomButton';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
-export {
-    Hero,
-    CustomButton
-}
+export { Hero, CustomButton, NavBar, Footer };
