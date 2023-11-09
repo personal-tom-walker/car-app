716a55b998761afe03947517acb7ebd4ae187827
tom-walker <walkertom1996@gmail.com>
2023-11-09 13:11:11 +0000

    main page content started - customFilter & searchBar components created

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({ title }: CustomFilterProps) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
