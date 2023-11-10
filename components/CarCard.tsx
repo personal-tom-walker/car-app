cfa6409f13a2566afd9a6ba582873eae869623f3
tom-walker <walkertom1996@gmail.com>
2023-11-10 17:28:50 +0000

    car details component created with typed props

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 
 import { CarProps } from '@/types';
-import { CustomButton } from '.';
+import { CarDetails, CustomButton } from '.';
 import { calculateCarRent } from '@/utils';
 
 interface CarCardProps {
@@ -77,6 +77,7 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+          <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
 };
