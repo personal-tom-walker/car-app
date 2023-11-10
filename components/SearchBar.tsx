1c35b71ad8e7d8cc025ede48ffb9e6a23924e4b0
tom-walker <walkertom1996@gmail.com>
2023-11-10 19:20:19 +0000

    search button created

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 import SearchManufacturer from './SearchManufacturer';
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src='/magnifying-glass.svg'
+      alt='magnifying glass'
+      width={40}
+      height={40}
+      className='object-conatin'
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
@@ -14,6 +27,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses='sm:hidden' />
       </div>
     </form>
   );
