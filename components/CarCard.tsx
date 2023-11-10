bb93061698f4fcefecef778029d00fd1b4acdceb
tom-walker <walkertom1996@gmail.com>
2023-11-10 17:02:47 +0000

    transmission, drive and mpg elements written for car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -12,21 +12,76 @@ interface CarCardProps {
 }
 
 const CarCard = ({ car }: CarCardProps) => {
-    const { city_mpg, year, make, model, transmission, drive } = car;
-    
-    const carRent = calculateCarRent(city_mpg, year);
-    return <div className="car-card group">
-        <div className="car-card__content">
-            <h2 className="car-card__content-title">
-                { make } { model }
-            </h2>
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const automaticTransmissionNotation = 'a';
+
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className='car-card group'>
+      <div className='car-card__content'>
+        <h2 className='car-card__content-title'>
+          {make} {model}
+        </h2>
+      </div>
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        £<span className='self-start text-[14px] font-semibold'></span>
+        {carRent}
+        <span className='self-end text-[14px] font-semibold'>/day</span>
+      </p>
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image
+          src='/hero.png'
+          alt={`${make} ${model} car`}
+          fill
+          priority
+          className='object-contain'
+        />
+      </div>
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image
+              src='/steering-wheel.svg'
+              alt={'steering wheel'}
+              width={20}
+              height={20}
+            />
+            <p className='text-[14px]'>
+              {transmission === automaticTransmissionNotation
+                ? 'Automatic'
+                : 'Manual'}
+            </p>
+          </div>
         </div>
-        <p>
-            <span>
-                {carRent}
-            </span>
-        </p>
-  </div>;
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image
+              src='/tire.svg'
+              alt={'tyre'}
+              width={20}
+              height={20}
+            />
+            <p className='text-[14px]'>
+              {drive.toUpperCase()}
+            </p>
+          </div>
+        </div>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image
+              src='/gas.svg'
+              alt={'fuel'}
+              width={20}
+              height={20}
+            />
+            <p className='text-[14px]'>
+              {city_mpg} MPG
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default CarCard
+export default CarCard;
